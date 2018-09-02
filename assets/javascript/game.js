3ba0c886c383a1729a8cb8a896a6b9e8a34282a0
Curly Joe <curlyjoe71@gmail.com>
2018-09-02 09:43:01 -0700

    Edited trivia files, along with soundfiles.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,49 +43,47 @@ var answerArray = [];
 
 //Creating a counter.
 var roundCounter = 1;
-var currentQuestionArray = []; 
+var currentQuestionArray = [];
+var answerDisplay = $('.answer-child');
 
 //Using the Fisher-Yates Shuffle after first shifting the "question" key off.
 var shuffleFunction = function (myKeys) {
 
   var shuffledArray = Object.keys(myKeys);
-  console.log(shuffledArray);
   if (shuffledArray[0] === 'question') {
     shuffledArray.shift();
   };
   answerArray = shuffledArray;
   for (var i = answerArray.length - 1; i >= 0; i--) {
     var randomIndex = Math.floor(Math.random() * (i + 1));
-    console.log(randomIndex);
     var itemAtIndex = answerArray[randomIndex];
-    console.log(itemAtIndex);
-
     answerArray[randomIndex] = answerArray[i];
     answerArray[i] = itemAtIndex;
   }
   return answerArray;
 };
-// console.log(question5);
-// console.log(shuffleFunction(question5));
 
 var timerDisplay = $('#timer-display');
 var slidingObject = $('#sliding-object');
 var questionDisplay = $('#questionDiv');
+
 
 //function to run when timer gets to zero.
 var timeOut = function () {
   stopwatch.stop();
   slidingObject.empty();
   questionDisplay.empty();
+  $('#answer-display').empty();
   timerDisplay.text('Oh no. You\'ve run out of time!');
 };
 
-//variables and included function to run and display the timer.
+//variables and included functions to run and display the timer.
 var currentTime;
 var intervalId;
 var clockRunning = false;
+
 var stopwatch = {
-  time: 120,
+  time: 60,
   reset: function () {
     stopwatch.time = 0;
     $('#timer-display').text('00:00');
@@ -99,7 +97,7 @@ var stopwatch = {
   stop: function () {
     clearInterval(intervalID);
     clockRunning = false;
-    stopwatch.time = 5;
+    // stopwatch.time = 5;
   },
   count: function () {
     if (stopwatch.time === 0) {
@@ -115,23 +113,63 @@ var stopwatch = {
   timeConverter: function (t) {
     var minutes = Math.floor(t / 60);
     var seconds = t - (minutes * 60);
-
     if (seconds < 10) {
       seconds = "0" + seconds;
     }
-
     if (minutes === 0) {
       minutes = "00";
     }
-
     else if (minutes < 10) {
       minutes = "0" + minutes;
     }
-
     return minutes + ":" + seconds;
   }
 };
 
+var checkRound = function () {
+  if (roundCounter === 1) {
+    currentQuestionArray = question1;
+  }
+  else if (roundCounter === 2) {
+    currentQuestionArray = question2;
+  }
+  else if (roundCounter === 3) {
+    currentQuestionArray = question3;
+  }
+  else if (roundCounter === 4) {
+    currentQuestionArray = question4;
+  }
+  else if (roundCounter === 5) {
+    currentQuestionArray === question5;
+  }
+  else {
+    //need to run a function to end the game
+  }
+};
+
+var playGame = function () {
+  checkRound();
+  stopwatch.start();
+  slidingObject.text('o');
+  timerDisplay.text(currentTime);
+
+  shuffleFunction(currentQuestionArray);
+  roundCounter++;
+  questionDisplay.text(currentQuestionArray.question);
+  $('#answer-display').children().text('');
+  setTimeout(function () {
+    $('#aDiv').text(currentQuestionArray[answerArray[0]]).attr('class', 'tracking-in-expand-fwd answer-child pointer hover');
+  }, 1500);
+  setTimeout(function () {
+    $('#bDiv').text(currentQuestionArray[answerArray[1]]).attr('class', 'tracking-in-expand-fwd answer-child pointer hover');
+  }, 2000);
+  setTimeout(function () {
+    $('#cDiv').text(currentQuestionArray[answerArray[2]]).attr('class', 'tracking-in-expand-fwd answer-child pointer hover');
+  }, 2500);
+  setTimeout(function () {
+    $('#dDiv').text(currentQuestionArray[answerArray[3]]).attr('class', 'tracking-in-expand-fwd answer-child pointer hover');
+  }, 2500);
+};
 
 $(document).ready(function () {
   slidingObject.empty();
@@ -139,41 +177,42 @@ $(document).ready(function () {
     timerDisplay.text('Welcome! Go ahead and click the "Start" button when you\'re ready to play!');
 
     $('.btn').click(function () {
+      $('.button-spacer').empty();
+      playGame();
 
-      if (roundCounter === 1) {
-        currentQuestionArray = question1;
-      }
-      else if (roundCounter === 2) {
-        currentQuestionArray = question2;
-      }
-      else if (roundCounter === 3) {
-        currentQuestionArray = question3;
-      }
-      else if (roundCounter === 4 ) {
-        currentQuestionArray = question4;
-      }
-      else {
-        currentQuestionArray === 5;
-      };
+      // checkAnswer();
+      $('.answer').click(function () {
+        if ($(this).text() === currentQuestionArray.right) {
+          stopwatch.stop();
+          slidingObject.empty();
+          questionDisplay.text('Correct!');
 
-      shuffleFunction(currentQuestionArray);
-      console.log(answerArray);
-      $('#questionDiv').text(currentQuestionArray.question);
-      setTimeout(function() {
-        $('#aDiv').text('A. ' + currentQuestionArray[answerArray[0]]).attr('class', 'tracking-in-expand-fwd answer-child pointer');
-      }, 100);
-      setTimeout(function() {
-        $('#bDiv').text('B. ' + currentQuestionArray[answerArray[1]]).attr('class', 'tracking-in-expand-fwd answer-child pointer');
-      }, 400);
-      setTimeout(function() {
-        $('#cDiv').text('C. ' + currentQuestionArray[answerArray[2]]).attr('class', 'tracking-in-expand-fwd answer-child pointer');
-      }, 700);
-      setTimeout(function() {
-        $('#dDiv').text('D. ' + currentQuestionArray[answerArray[3]]).attr('class', 'tracking-in-expand-fwd answer-child pointer');
-      }, 1000);
-      stopwatch.start();
-      slidingObject.text('o');
-      timerDisplay.text(currentTime);
+          // Is there a way to empty the other divs?
+          $('#answer-display').children().not($(this)).empty().removeClass('hover');
+          setTimeout(() => {
+            questionDisplay.text('Next question coming up...');
+          }, 1900);
+          setTimeout(() => {
+            playGame();
+          }, 4000);
+        }
+        else {
+          stopwatch.stop();
+          slidingObject.empty();
+          questionDisplay.text('Oh, sorry. That\'s not correct.');
+          $('#answer-display').children().not($(this)).empty();
+          setTimeout(() => {
+            questionDisplay.text('This is the one you wanted...');
+            $(this).text(currentQuestionArray.right).css('color', 'red');
+          }, 2000);
+          setTimeout(() => {
+            questionDisplay.text('Next question coming up...');
+          }, 5000);
+          setTimeout(() => {
+            playGame();
+          }, 7100);
+        }
+      });
     })
 
 
